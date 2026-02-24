2085190645c9271ae7d957b1dafd4605e09c397c
Yogesh Mehta <r.yogeshmehta@gmail.com>
2026-02-24 13:55:12 +0530

    Update UseCase2PalindromeCheckerApp.java

--- UseCase2PalindromeCheckerApp.java
+++ UseCase2PalindromeCheckerApp.java
@@ -1,9 +1,7 @@
 public class UseCase2PalindromeCheckerApp {
 
-    // Application Version
     private static final String APP_VERSION = "1.1.0";
 
-    // Main Method - Entry Point
     public static void main(String[] args) {
 
         System.out.println("====================================");
@@ -13,19 +11,18 @@ public class UseCase2PalindromeCheckerApp {
         System.out.println("Application Version : " + APP_VERSION);
         System.out.println("====================================");
 
-        // Hardcoded String Literal
+        
         String word = "madam";
 
         System.out.println("Checking word: " + word);
 
-        // Reverse the string manually
+        
         String reversedWord = "";
 
         for (int i = word.length() - 1; i >= 0; i--) {
             reversedWord = reversedWord + word.charAt(i);
         }
-
-        // Conditional Statement
+        
         if (word.equals(reversedWord)) {
             System.out.println(word + " is a Palindrome.");
         } else {
