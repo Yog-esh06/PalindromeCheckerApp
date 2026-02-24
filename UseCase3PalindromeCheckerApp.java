442c4d79d2c7691f21a49fb3fe85eedaae5c0f15
Yogesh Mehta <r.yogeshmehta@gmail.com>
2026-02-24 13:56:15 +0530

    Update UseCase3PalindromeCheckerApp.java

--- UseCase3PalindromeCheckerApp.java
+++ UseCase3PalindromeCheckerApp.java
@@ -1,45 +1,10 @@
-/**
- * MAIN CLASS UseCase3PalindromeCheckerApp
- *
- * Use Case 3: Reverse String Based Palindrome Check
- *
- * Description:
- * This class checks whether a string is a palindrome
- * by reversing the string and comparing it with
- * the original value.
- *
- * At this stage, the application:
- * - Iterates the string in reverse order
- * - Builds a reversed version
- * - Compares original and reversed strings
- * - Displays the validation result
- *
- * This introduces transformation-based validation.
- *
- * @author Developer
- * @version 3.6
- */
-
 public class UseCase3PalindromeCheckerApp {
-
-    /**
-     * Application entry point for UC3.
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
-
-        // Hardcoded string
         String original = "madam";
-
-        // Variable to store reversed string
         String reversed = "";
-
-        // Reverse the string using for loop
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed = reversed + original.charAt(i);
         }
-
-        // Compare original and reversed string
         if (original.equals(reversed)) {
             System.out.println("The given string \"" + original + "\" is a PALINDROME.");
         } else {
